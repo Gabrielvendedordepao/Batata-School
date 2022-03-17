5e0e42c503e4a3601505131220a113397d201c37
Gabrielvendedordepao <gabriel.spontao10@gmail.com>
2022-03-17 17:22:43 -0300

    criando os usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
   resources :turmas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
