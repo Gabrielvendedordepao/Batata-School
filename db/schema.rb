c7e6c6b54a1da5a7badab5b123f5897d3f9aa869
Gabrielvendedordepao <gabriel.spontao10@gmail.com>
2022-03-31 17:31:12 -0300

    montamos o banco de dados de dados e editamos o tweet criado hoje

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_17_193032) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_31_193315) do
   create_table "turmas", force: :cascade do |t|
     t.string "nome"
     t.datetime "inicio"
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_17_193032) do
     t.float "horas"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "conteudo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_17_193032) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
